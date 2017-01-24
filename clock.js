3a5404bc664730962388f8c02c00e824498d361c
Shimei Wong <wshimei@gmail.com>
2017-01-24 16:28:12 +0800

    tick the second 45deg

---
+++ clock.js
@@ -0,0 +1,20 @@
+var second = document.getElementById('second')
+var minute = document.querySelector('#minute')
+var hour = document.querySelector('#hour')
+
+var degrees = 45
+
+// second.style.transform = "rotate(" + degrees + "deg)";
+
+
+
+// var degrees = 45;
+//
+// var el = document.getElementById("myElement");
+// el.style.transform = "rotate(" + degrees + "deg)";
+
+// secondRotation(0); // returns 0
+// secondRotation(59); // returns (59 / 60) * 360
+//
+// hourRotation(12); // returns 0
+// hourRotation(5); // returns (5 / 12) * 360
