84d8e74ab2cf0ab51f46eed41acc8c189f21a08d
Shimei Wong <wshimei@gmail.com>
2017-01-25 11:47:53 +0800

    got clock to update by interval

--- clock.js
+++ clock.js
@@ -1,31 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var now = new Date()
-  var seconds = now.getSeconds()
-  var minutes = now.getMinutes()
-  var hours = now.getHours()
-  // var second = document.getElementById('second')
-  // var minute = document.getElementById('minute')
-  // var hour = document.getElementById('hour')
+  function startClock () {}
+    var now = new Date()
+    var seconds = now.getSeconds()
+    var minutes = now.getMinutes()
+    var hours = now.getHours()
 
-  function runningSecond (sec) {
-    var degrees = sec / 60 * 360
-    second.style.transform = 'rotate(' + degrees + 'deg)'
+    function runningSecond (sec) {
+      var degrees = sec / 60 * 360
+      second.style.transform = 'rotate(' + degrees + 'deg)'
+    }
+    runningSecond(seconds)
+
+    function runningMinute (min) {
+      var degrees = min / 60 * 360
+      minute.style.transform = 'rotate(' + degrees + 'deg)'
+    }
+    runningMinute(minutes)
+
+    function runningHour (hr) {
+      var degrees = hr / 12 * 360
+      hour.style.transform = 'rotate(' + degrees + 'deg)'
+    }
+    runningHour(hours)
   }
-  runningSecond(seconds)
-
-  function runningMinute (min) {
-    var degrees = min / 60 * 360
-    minute.style.transform = 'rotate(' + degrees + 'deg)'
-  }
-  runningMinute(minutes)
-
-  function runningHour (hr) {
-    var degrees = hr / 12 * 360
-    hour.style.transform = 'rotate(' + degrees + 'deg)'
-  }
-  runningHour(hours)
-
-  setInterval(function () {
-    location = ''
-  }, 1000)
+  setInterval(startClock, 1000)
 })
